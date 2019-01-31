5b4ab282bd65c64651931b690cae835cba296919
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-01-31 14:35:01 +0530

    components part 1; generated menu component, addedto app.module, css and wrote alt ad image linking

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { MenuComponent } from './menu/menu.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
