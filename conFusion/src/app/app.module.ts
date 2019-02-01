069b0b36d7bc3d9ce1beb92e2c36c19adcb84241
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-02-01 15:54:58 +0530

    generate dish detail

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -14,12 +14,14 @@ import { AppComponent } from './app.component';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    DishdetailComponent,
   ],
   imports: [
     BrowserModule,
