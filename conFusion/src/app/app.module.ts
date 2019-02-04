677dde7e96ed667802857143f1d96a6b11420a0d
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-02-04 16:33:09 +0530

    generate about, contact and home compnenents

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -19,7 +19,10 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import{ DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component'
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component'
 
 @NgModule({
   declarations: [
@@ -28,6 +31,9 @@ import { FooterComponent } from './footer/footer.component'
     DishdetailComponent,
     HeaderComponent,
     FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
