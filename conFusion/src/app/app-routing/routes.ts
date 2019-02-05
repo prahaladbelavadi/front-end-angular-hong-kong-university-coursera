8ed6485be3f2d7118505d8b90ece8c19316c6388
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-02-05 16:12:22 +0530

    single page application part 1; integrated contact, home components and dish and promotion serivices

--- conFusion/src/app/app-routing/routes.ts
+++ conFusion/src/app/app-routing/routes.ts
@@ -9,8 +9,9 @@ import { ContactComponent } from '../contact/contact.component'
 
 
 export const routes: Routes =[
-   { path: 'home', component: HomeComponent },
-   { path: 'menu', component: MenuComponent },
-   { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+    { path: 'home', component: HomeComponent },
+    { path: 'menu', component: MenuComponent },
+    { path: 'contactus', component: ContactComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, 
    
 ]
