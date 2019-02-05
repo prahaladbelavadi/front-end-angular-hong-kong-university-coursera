36c67494db1dda3f75012e1a972304b4bbe13b7f
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-02-05 13:09:22 +0530

    angular router basics

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -13,16 +13,18 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-
-import{ DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component'
+
+import { DishService } from './services/dish.service';
+
+import {AppRoutingModule} from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -40,6 +42,7 @@ import { ContactComponent } from './contact/contact.component'
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
+    AppRoutingModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
