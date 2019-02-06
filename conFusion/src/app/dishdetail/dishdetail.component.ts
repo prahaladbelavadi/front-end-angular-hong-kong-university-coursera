01a39d0f8459b61074dcab52070ef260b926ef6c
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-02-07 00:45:50 +0530

    the dish detail part is not working; the footber link is workingn

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
 
 
 @Component({
@@ -7,14 +11,22 @@ import { Dish } from '../shared/dish';
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss']
 })
+
 export class DishdetailComponent implements OnInit {
 
-  @Input()
-  dish = Dish;
+  dish: Dish;
 
-  constructor() { }
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
