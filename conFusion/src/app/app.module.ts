cb9a43bfd0781bf0bddf05763b83fdaa56ff9bb7
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-02-11 13:44:06 +0530

    assignment2: task1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { ContactComponent } from './contact/contact.component'
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 import {AppRoutingModule} from './app-routing/app-routing.module';
 
@@ -50,7 +51,7 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
     MatButtonModule
   ],
   providers: [
-    DishService, PromotionService
+    DishService, PromotionService, LeaderService
   ],
   bootstrap: [AppComponent]
 })
