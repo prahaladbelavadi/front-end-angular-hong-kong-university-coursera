c1040fe0cb291dfbe734ade1a5debf2b0def17a8
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-02-12 17:41:36 +0530

    Template driven forms part 1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatOptionModule } from '@angular/material';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule, MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -48,14 +52,18 @@ import { LoginComponent } from './login/login.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
+    FormsModule,
     AppRoutingModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
     MatOptionModule,
-    MatDialogModule
-  ],
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule
+    ],
   providers: [
     DishService, PromotionService, LeaderService
   ],
