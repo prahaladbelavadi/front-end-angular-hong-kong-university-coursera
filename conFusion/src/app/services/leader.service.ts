0bb5b7d647858845b603981e6d0e90f6ee17c92a
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-02-15 13:01:51 +0530

    Promise Part 1: the assignment part is kinda screwing up because the promise isn not returnning properly. Threee errors: resolve does not exist on type; need to degub

--- conFusion/src/app/services/leader.service.ts
+++ conFusion/src/app/services/leader.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { LEADERS } from '../shared/leaders';
 import {Leader} from '../shared/leader'
+import { promise } from 'protractor';
+import { Promise } from 'q';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +11,15 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeaders(): Leader[]{
-    return  LEADERS
+  getLeaders(): Promise<Leader[]>{
+    return Promise.resolve(LEADERS);
   }
 
-  getLeader(id: string): Leader {
-    return LEADERS.filter((leader) => (leader.id === id))[0];
+  getLeader(id: string): Promise<Leader> {
+    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
   }
   
-  getFeaturedLeader(): Leader{
-    return LEADERS.filter((leader) => leader.featured)[0];
+  getFeaturedLeader(): Promise<Leader>{
+    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
   }
 }
