5e2a4097758309c44aeab282ba3a2a24681b666a
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-02-16 17:42:10 +0530

    Promise Part 2; Everything is working. Yay! ; added delay(probably the only time i'd  do this) to all services and modified the HTML components to use spinners in their place as they load accordingly

--- conFusion/src/app/services/leader.service.ts
+++ conFusion/src/app/services/leader.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { LEADERS } from '../shared/leaders';
 import {Leader} from '../shared/leader'
+import { resolve } from 'q';
 
 
 @Injectable({
@@ -11,14 +12,26 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]>{
-    return Promise.resolve(LEADERS);
+    return new Promise(resolve=>{
+      setTimeout(() => {
+        resolve((LEADERS))
+      }, 2000);
+    })
   }
 
   getLeader(id: string): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+    return new Promise(resolve=>{
+      setTimeout(() => {
+        resolve(LEADERS.filter((leader) => (leader.id === id))[0])
+      }, 2000);
+    });
   }
   
   getFeaturedLeader(): Promise<Leader>{
-    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+    return new Promise(resolve=>{
+      setTimeout(() => {
+        resolve(LEADERS.filter((leader) => leader.featured)[0]);
+      }, 2000);
+    })
   }
 }
