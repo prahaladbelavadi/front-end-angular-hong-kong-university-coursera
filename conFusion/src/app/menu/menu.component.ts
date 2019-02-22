42080614ec2c1e154736f764ec6be744a97ea8ef
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-02-22 12:50:03 +0530

    fix http 1; it works now; I was testing the baseUrl constructor thing and forgot to change it back; need to verify http2

--- conFusion/src/app/menu/menu.component.ts
+++ conFusion/src/app/menu/menu.component.ts
@@ -15,7 +15,7 @@ export class MenuComponent implements OnInit {
 
 
   constructor(private dishService: DishService,
-    @Inject('baseURL') private baseURL) { }
+    @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
     this.dishService.getDishes()
